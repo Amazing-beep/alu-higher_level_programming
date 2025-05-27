b015579df977f53e6620f0dd615064fba272cd3f
Amazing-beep <>
2025-05-27 06:21:44 -0400

    Add 3-rectangle.js file with print() method

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    }
+    // If w or h is not a positive integer, the object remains empty
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        let row = "";
+        for (let j = 0; j < this.width; j++) {
+          row += "X";
+        }
+        console.log(row);
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
